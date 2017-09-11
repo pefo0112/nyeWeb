411e5faf5aa6b147ef566e0156515ad997b490b1
Petter Foss <petter-foss@hotmail.com>
2017-09-11 19:03:39 +0200

    Endret i noen filer, some minor changes, bug fixes etc.. add img of bernt

--- reactmappe/src/App.js
+++ reactmappe/src/App.js
@@ -2,17 +2,64 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+}
+const petter = {
+  firstName: 'Petter',
+  lastName: 'Foss'
+}
+const header = React.createElement(
+  'h1',
+  {className: 'Greeting'},
+  'Hello World'
+)
+
+function tick() {
+  const element = (
+    <div>
+      <h1>
+        Klokke
+      </h1>
+      <h2>
+        It is {new Date().toLocaleTimeString()}
+      </h2>
+    </div>
+  );
+  return element;
+}
+
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {klipper: "Nei"}
+
+  }
+  handleButtonClick(){
+    if (this.state.klipper === "Ja") {
+      this.setState({klipper: "Nei"})
+    }else {
+      this.setState({klipper: "Ja"})
+    }
+  }
   render() {
+
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <h2>Sia til </h2><h1><b>Bernt</b></h1>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code>
         </p>
+        <h3>
+          {this.state.klipper}
+          <button onClick={this.handleButtonClick.bind(this)}>Kappen</button>
+          Hello, {formatName(petter)}!
+          {tick()}
+        </h3>
         <div className="footer">
+          {header}
           <h3>
             ReactJS er et javascript framework
           </h3>
